65eaf7f22cf3134822ace04eddf52aee2656a5fa
mohnishgorana1 <mohnishgorana1@gmail.com>
2023-08-16 12:30:38 +0530

    hero section started

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 
 import './App.css'
 import Navbar from './components/Navbar'
+import Hero from './components/Hero'
 
 function App() {
 
@@ -10,6 +11,7 @@ function App() {
     <>
       <div>
         <Navbar />
+        <Hero />
       </div>
     </>
   )
