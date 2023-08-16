90cce6a48cc50bbabca89e0469c2564075af8f69
mohnishgorana1 <mohnishgorana1@gmail.com>
2023-08-16 12:30:26 +0530

    navbar completed

---
+++ src/components/Hero.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+function Hero() {
+  return (
+    <div className='text-white'>
+       <div className='flex flex-col items-center justify-center max-w-[800px] mt-[-96px] w-full h-screen mx-auto'>
+        <p className='text-[#00df9a] font-bold p-2'>GROWING WITH DATA</p>
+        <h1 className='md:text-7xl sm:text-6xl text-4xl font-bold'>Grow with data</h1>  
+        <div>
+            <p>Fast, Flexible financing for </p>
+        </div>
+
+       </div>
+    </div>
+  )
+}
+
+export default Hero
